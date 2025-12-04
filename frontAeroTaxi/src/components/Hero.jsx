93228cfde4c7a46acaf09e78a8cd100346169728
SergioTobon <sestebantmontoya@gmail.com>
2025-12-03 20:21:43 -0500

    no message

--- frontAeroTaxi/src/components/Hero.jsx
+++ frontAeroTaxi/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import "../styles/styleHero.css"
 function Hero() {
   return (
     <section className="hero" id="hero">
-      <h1>juan es gay</h1>
+      <h1>Tu transporte seguro desde el aeropuerto</h1>
       <p>Reserva tu taxi aéreo en México antes de viajar y llega a tu destino sin complicaciones.</p>
       <a href="#services" className="btn-primary">Conoce nuestros servicios</a>
     </section>
